dd33fe7ad3cddbb506e57a09a2db090ef14432ba
Ubuntu <ubuntu@ip-172-31-26-241.ap-southeast-1.compute.internal>
2024-04-08 10:44:55 +0000

    updated dockerfile

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 docker build -t myimage .
-<<<<<<< HEAD
+
 docker-compose up -d 
-=======
-docker-compose up -d
->>>>>>> cdb9573 (Updated build.sh and deploy.sh)
+
 echo "The build is successful"
 
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-docker login -u $Username -p Password
+docker login -u $Username -p $Password
 
-if [GIT_BRANCH == "origin/dev"]; then
-docker tag
-docker push vennila1011/dev:latest
-elif [GIT_BRANCH == "origin/master"]; then
-docker tag
-docker push vennila1011/prod:latest
+if [ "$GIT_BRANCH" == "origin/dev" ]; then
+docker tag myimage vennila1011/myimage:dev
+docker push vennila1011/dev
+elif [ "$GIT_BRANCH" == "origin/master" ]; then
+docker tag myimage vennila1011/myimage:prod
+docker push vennila1011/prod
 else 
-echo"Image is not pushed in DockerHub"
+echo "Image is not pushed in DockerHub"
 fi
 
 
