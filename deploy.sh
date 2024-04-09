467a03f3f87cb97d31fc3a4c4c9540eb455a898f
Vennila1011 <157454398+Vennila1011@users.noreply.github.com>
2024-04-09 09:38:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 docker login -u vennila1995 -p dckr_pat_AjZmebMzfg0Ghh3nl9Hjn0zoB0E
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$GIT_BRANCH" == "dev" ]; then
         echo "In Dev branch"
 docker tag myimage vennila1995/dev
 docker push vennila1995/dev
-elif [ "$GIT_BRANCH" == "origin/master" ]; then
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
 	echo "In Prod Branch"
 docker tag myimage vennila1995/prod
 docker push vennila1995/prod
