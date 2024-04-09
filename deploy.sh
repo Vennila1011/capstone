fb7169904cbf2839e58cb82a579fcd3b36794f6c
Vennila1011 <157454398+Vennila1011@users.noreply.github.com>
2024-04-10 00:56:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ docker login -u vennila1995 -p dckr_pat_AjZmebMzfg0Ghh3nl9Hjn0zoB0E
 
 if [[ $GIT_BRANCH == "dev" ]]; then
         echo "In Dev branch"
-docker tag myimage vennila1995/dev
+docker tag my_reactapp_image vennila1995/dev
 docker push vennila1995/dev
 elif [[ $GIT_BRANCH == "master" ]]; then
 	echo "In Prod Branch"
-docker tag myimage vennila1995/prod
+docker tag my_reactapp_image vennila1995/prod
 docker push vennila1995/prod
 else
 echo "Image is not pushed in DockerHub."
