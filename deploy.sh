c55f5389beb9a17d9cc5c918873fbd21fb0cf776
Vennila1011 <157454398+Vennila1011@users.noreply.github.com>
2024-04-10 02:11:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@
 # Docker login
 
 docker login -u vennila1995 -p dckr_pat_AjZmebMzfg0Ghh3nl9Hjn0zoB0E
-
-GIT_BRANCH=$(git branch --show-current)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Current Git branch: $GIT_BRANCH"
 # Check the Git branch
 
@@ -13,7 +12,7 @@ if [[ $GIT_BRANCH == "dev" ]]; then
 docker tag my_reactapp_image vennila1995/dev
 docker push vennila1995/dev
 elif [[ $GIT_BRANCH == "master" ]]; then
-	echo "In Prod Branch"
+        echo "In Prod Branch"
 docker tag my_reactapp_image vennila1995/prod
 docker push vennila1995/prod
 else
