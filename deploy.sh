527e1fb2337c1f7f2a6cf601a948dcd702053c2f
Vennila1011 <157454398+Vennila1011@users.noreply.github.com>
2024-04-10 02:22:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker login -u vennila1995 -p dckr_pat_AjZmebMzfg0Ghh3nl9Hjn0zoB0E
 echo "Current Git branch: $GIT_BRANCH"
 # Check the Git branch
 
-if [[ $GIT_BRANCH == "dev" ]]; then
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
         echo "In Dev branch"
 docker tag my_reactapp_image vennila1995/dev
 docker push vennila1995/dev
